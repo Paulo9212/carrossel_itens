de47e9a322be90617dccf6b71461ee19be300798
Paulo_Rb <papalohp7@gmail.com>
2022-08-11 19:24:22 -0300

    11/08

---
+++ script.js
@@ -0,0 +1,12 @@
+//EFEITO PISCAR (TICKETS)
+$(".product-grid1").mouseover(function() {
+    $( ".ticket_new" ).fadeOut(400);
+    $( ".ticket_new" ).fadeIn(400);
+});
+
+$(".product-grid1").mouseover(function() {
+    $( ".ticket_sale" ).fadeOut(400);
+    $( ".ticket_sale" ).fadeIn(400);
+});
+
+
